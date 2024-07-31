7862bffad88e18f0367bbcae640ad7670440f96e
Kai Vedder <kawasaki.vedder@gmail.com>
2024-07-31 17:18:27 -0400

    fixed project name

---
+++ src/gapBuffer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by itska on 7/31/2024.
+//
+
+#include "gapBuffer.h"
---
+++ src/gapBuffer.h
@@ -0,0 +1,14 @@
+//
+// Created by itska on 7/31/2024.
+//
+
+#ifndef PROJECT3_GAPBUFFER_H
+#define PROJECT3_GAPBUFFER_H
+
+
+class gapBuffer {
+
+};
+
+
+#endif //PROJECT3_GAPBUFFER_H
